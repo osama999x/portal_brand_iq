c83af0ee9036d4e1176a86b705b97ff3f219773f
Muhammad Hamza <muhammad.hamza@appinsnap.com>
2023-11-06 17:45:57 +0500

    duplicate

--- src/utilities/Config.js
+++ src/utilities/Config.js
@@ -1,6 +1,7 @@
 //  let baseURL = process.env.NODE_ENV === "development" ? process.env.REACT_APP_BASE_URL_DEV : process.env.REACT_APP_BASE_URL_LIVE;
- let baseURL = "http://20.212.227.60:3015/"
-//let baseURL = "http://192.168.4.60:3006/"
+// let baseURL = "http://20.212.227.60:3015/"
+let baseURL = "http://20.212.227.60:3015/"
+
 
 export { baseURL };
 // 
